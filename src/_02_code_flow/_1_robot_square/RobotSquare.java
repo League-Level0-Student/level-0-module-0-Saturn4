12469a77d1c5138f9f6d59b78dc4eda838fd3822
student <student@ip-172-31-54-9.us-west-2.compute.internal>
2021-05-24 18:28:37 -0700

    Eclipse Robots

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,29 +17,25 @@ public class RobotSquare {
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
-
+rob.penDown();
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed();    **/
- 
+     rob.setSpeed(100);
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
-    	
-
+    int count = 0;	
+    
+     while (count < 4) {
         // DRAW.  Draw a line. Use this command:
     		  /**     rob.move();    **/
-  
-
+    	 rob.move(100);
 	// TURN.  Turn 90 degrees. Use this command:
     		  /**     rob.turn();    **/
-
+    	 rob.turn(90);
     	
-    	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
-    		  /**     count += 1;    **/
-
-    	
-    	// Run the program. Did the Robot draw a line and turn?
-    	 
+    	 count++;
+     	}
     	
         // LOOP.  Make a while loop and move the DRAW, TURN, and INCREASE COUNT code 
     	//        so it is between the curly braces like the example below. 
